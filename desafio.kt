5556dc7c49ad833e0b60fd27943120ed54311682
Stein <143040368+steinth@users.noreply.github.com>
2024-08-31 21:43:53 -0300

    melhorando o codigo

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,42 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
+class Usuario(val nome: String, val cpf: Long){
+}
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome}, portador do CPF ${usuario.cpf}, foi matriculado no curso $nome.")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Thiago", 51822601929)
+    val usuario2 = Usuario("Andre", 39600481707)
+    
+    val conteudo1 = ConteudoEducacional("Tratamento de exceção em kotlin", 2)
+    val conteudo2 = ConteudoEducacional("Dominando kotlin", 2)
+    
+    val formacao1 = Formacao("analise e desenvolvimento de sistemas", listOf(conteudo1), Nivel.AVANCADO)
+    val formacao2 = Formacao(nome = "kotlin iniciante", conteudos = listOf(conteudo1, conteudo2), nivel = Nivel.INTERMEDIARIO)
+    formacao1.matricular(usuario1)
+    formacao2.matricular(usuario2)
+    
+    println("inscritos na formação de ${formacao1.nome}")
+    for (usuario in formacao1.inscritos){
+        println("NOME:  ${usuario.nome}")
+        println("CPF:  ${usuario.cpf}")
+        println("Nivel: ${formacao1.nivel}")
+    }
+    println("inscritos na formação de ${formacao2.nome}")
+    for (usuario in formacao2.inscritos){
+        println("NOME:  ${usuario.nome}")
+        println("CPF:  ${usuario.cpf}")
+        println("Nivel: ${formacao2.nivel}")
+    }    
 }
